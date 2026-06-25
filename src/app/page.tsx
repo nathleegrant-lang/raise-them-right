9866a37dcc4a62cbea1f6441067c4974996c9a83
nathleegrant-lang <nathleegrant@gmail.com>
2026-06-24 19:44:43 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
           <div className="hero-overlay" />
           <div className="hero-content">
             <p className="eyebrow">A Public Awareness Initiative by Nathlee R. Grant</p>
-            <h1>It takes a community to raise a child.</h1>
+            <h1>It takes a <br>community to </br><br>raise a child.</br></h1>
             <p>
               Children are always watching, always listening, and always learning.
               #RaiseThemRight is a call for all of us to play our part well.
