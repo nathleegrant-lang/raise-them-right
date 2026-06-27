70bc4c0efce2688a103a33b0cabe9adcd777ea52
nathleegrant-lang <nathleegrant@gmail.com>
2026-06-26 23:43:02 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-export const metadata: Metadata = {
-  title: "#RaiseThemRight | Strong Homes. Strong Children. Strong Nation.",
-  description:
-    "#RaiseThemRight is a public awareness initiative by Nathlee R. Grant calling families, communities, schools, churches, and everyday citizens to help shape the next generation.",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+
+        {children}
+
+        <Footer />
+      </body>
     </html>
   );
 }
