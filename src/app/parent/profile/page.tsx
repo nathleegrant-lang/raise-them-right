3f1297b0af94b3857976c6c949b08babb88b4f60
nathleegrant-lang <nathleegrant@gmail.com>
2026-08-16 19:08:47 -0500

    Add member sign out to Parent profile

--- src/app/parent/profile/page.tsx
+++ src/app/parent/profile/page.tsx
@@ -1,7 +1,15 @@
+import MemberSignOut from "../../../components/MemberSignOut";
 import { requireMember } from "../../../lib/memberAuth";
 import ParentProfileClient from "./ParentProfileClient";
 
 export default async function ParentProfilePage() {
   await requireMember("parent");
-  return <ParentProfileClient />;
+  return (
+    <>
+      <div style={{ maxWidth: "900px", margin: "1rem auto -0.5rem", padding: "0 1rem", display: "flex", justifyContent: "flex-end" }}>
+        <MemberSignOut />
+      </div>
+      <ParentProfileClient />
+    </>
+  );
 }
